a1fac8486c2d408381ea691ec635989fbab3b4a7
Shayan Guha <shayanguha@gmail.com>
2022-06-28 15:24:04 -0700

    Update QR code URL

--- src/url.ts
+++ src/url.ts
@@ -1,11 +1,11 @@
 import { ProjectFlavor } from './config';
 
 const generateQRCodeBaseURLForGo = 'https://api.qrserver.com/v1/create-qr-code/?size=512x512&data=exp://exp.host';
-const generateQRCodeBaseURLForBare = 'https://us-central1-exponentjs.cloudfunctions.net';
+const generateQRCodeBaseURLForBare = 'https://chart.googleapis.com/chart?cht=qr&chs=360x360&choe=UTF-8&chld=L|2'
 
 export function createQRCodeURL(projectFlavor: ProjectFlavor, manifestURL: string, scheme: string): string {
 	if (projectFlavor === ProjectFlavor.DevelopmentClient) {
-		return `${generateQRCodeBaseURLForBare}/generateQRCode/development-client?appScheme=${scheme}&url=${manifestURL}`;
+		return `${generateQRCodeBaseURLForBare}&chl=${scheme}://expo-development-client/?url=${manifestURL}`
 	} else if (projectFlavor === ProjectFlavor.ExpoGo) {
 		return `${generateQRCodeBaseURLForGo}/${manifestURL}`;
 	}
